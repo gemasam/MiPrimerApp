3e51dc074844b6fc09352a0724c843da3d87aa22
gemasam <gemasamantha94@gmail.com>
2022-02-07 11:01:54 -0600

    Task | Mi segundo commit | Gema S. Gomez.

--- MiPrimerApp/Program.cs
+++ MiPrimerApp/Program.cs
@@ -6,7 +6,7 @@ namespace MiPrimerApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo 2 ...");
         }
     }
 }
